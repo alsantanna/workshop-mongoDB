a3e9f300d324a03e32a37a84ca287bcfee25f0cd
Alex <al_santanna@yahoo.com>
2020-08-28 19:22:07 -0300

    Referenciando os posts do usuario

--- src/main/java/com/onlinemedia/workshopmongo/domain/Post.java
+++ src/main/java/com/onlinemedia/workshopmongo/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.onlinemedia.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -15,18 +17,18 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO authorDto;
 	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO authorDto) {
 		super();
 		this.id = id;
 		this.date = date;
 		this.title = title;
 		this.body = body;
-		this.author = author;
+		this.authorDto = authorDto;
 	}
 
 	public String getId() {
@@ -61,12 +63,12 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 
-	public User getAuthor() {
-		return author;
+	public AuthorDTO getAuthorDto() {
+		return authorDto;
 	}
 
-	public void setAuthor(User author) {
-		this.author = author;
+	public void setAuthorDto(AuthorDTO authorDto) {
+		this.authorDto = authorDto;
 	}
 
 	@Override
