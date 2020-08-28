6aac8b36d7664f07ebf9a69209eba3605b4d1d92
Alex <al_santanna@yahoo.com>
2020-08-28 02:35:52 -0300

    First Commit

---
+++ src/main/java/com/onlinemedia/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.onlinemedia.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
